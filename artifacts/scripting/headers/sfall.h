8878b30590dfdcf6a6bfc61a431967ac1f62a486
Mr.Stalin <Fakels@yandex.ru>
2021-01-13 19:41:39 +0300

    Updated solution file.

--- artifacts/scripting/headers/sfall.h
+++ artifacts/scripting/headers/sfall.h
@@ -81,7 +81,7 @@
 #define HOOK_TARGETOBJECT     (42)
 #define HOOK_ENCOUNTER        (43)
 #define HOOK_ADJUSTPOISON     (44)
-#define HOOK_ADJUSTRADIATION  (45)
+#define HOOK_ADJUSTRADS       (45)
 #define HOOK_ROLLCHECK        (46)
 
 //Valid arguments to list_begin
