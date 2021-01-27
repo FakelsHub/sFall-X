d869e6d05a512d2c964ad0ccd4e38d92c2a79b61
Mr.Stalin <Fakels@yandex.ru>
2021-01-28 00:41:29 +0300

    Added a developer tool.

---
+++ DevUtils/AddrHackScanner/AddrHackScanner/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace AddrHackScanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.tbStart = new System.Windows.Forms.TextBox();
+            this.tbEnd = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbResult
+            // 
+            this.tbResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbResult.Location = new System.Drawing.Point(12, 33);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbResult.Size = new System.Drawing.Size(682, 375);
+            this.tbResult.TabIndex = 0;
+            this.tbResult.WordWrap = false;
+            // 
+            // tbStart
+            // 
+            this.tbStart.Location = new System.Drawing.Point(50, 6);
+            this.tbStart.Name = "tbStart";
+            this.tbStart.Size = new System.Drawing.Size(100, 20);
+            this.tbStart.TabIndex = 1;
+            // 
+            // tbEnd
+            // 
+            this.tbEnd.Location = new System.Drawing.Point(196, 6);
+            this.tbEnd.Name = "tbEnd";
+            this.tbEnd.Size = new System.Drawing.Size(100, 20);
+            this.tbEnd.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(383, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFolder.Location = new System.Drawing.Point(619, 4);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnFolder.TabIndex = 4;
+            this.btnFolder.Text = "Folder";
+            this.btnFolder.UseVisualStyleBackColor = true;
+            this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Enabled = false;
+            this.btnSearch.Location = new System.Drawing.Point(302, 4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Start:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "End:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 420);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnFolder);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbEnd);
+            this.Controls.Add(this.tbStart);
+            this.Controls.Add(this.tbResult);
+            this.Name = "Form1";
+            this.Text = "Address Scanner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.TextBox tbStart;
+        private System.Windows.Forms.TextBox tbEnd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DevUtils/AddrHackScanner/AddrHackScanner/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AddrHackScanner
+{
+    public partial class Form1 : Form
+    {
+        string searchPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static void Find(string[] text, int start, int end, List<string> result)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                int s = 0;
+                do {
+                    int addr = SubFind(text[i], ref s);
+                    if (addr == 0) break;
+                    if (addr >= start && addr <= end) {
+                        // входит в диапазон
+                        result.Add("  Line: " + (i + 1) + " // " + text[i]);
+                    }
+                } while (s < text[i].Length);
+            }
+        }
+
+        private static int SubFind(string text, ref int s)
+        {
+            int addr = 0;
+            string addrHex = string.Empty;
+            int n = text.IndexOf("0x", s, StringComparison.OrdinalIgnoreCase);
+            if (n != -1) {
+                s = n + 2;
+                for (int m = s; m < text.Length; m++) {
+                    if (char.IsLetterOrDigit(text[m])) {
+                        addrHex += text[m];
+                    } else {
+                        s = m;
+                        break;
+                    }
+                }
+                Int32.TryParse(addrHex, System.Globalization.NumberStyles.HexNumber, null, out addr);
+            }
+            return addr;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int start, end;
+            if (!Int32.TryParse(tbStart.Text, System.Globalization.NumberStyles.HexNumber, null, out start)) {
+                return;
+            }
+            if (!Int32.TryParse(tbEnd.Text, System.Globalization.NumberStyles.HexNumber, null, out end)) {
+                return;
+            }
+            if (start > end) return;
+
+            List<string> files = Directory.GetFiles(searchPath, "*.cpp", SearchOption.AllDirectories).ToList();
+            files.AddRange(Directory.GetFiles(searchPath, "*.h", SearchOption.AllDirectories).ToList());
+
+            files.Sort();
+
+            List<string> result = new List<string>();
+
+            foreach (var file in files)
+            {
+                result.Add("File: " + file);
+                int count = result.Count;
+
+                Find(File.ReadAllLines(file), start, end, result);
+
+                if (count == result.Count) {
+                    result.RemoveAt(count - 1);
+                } else {
+                    result.Add(string.Empty);
+                }
+            }
+
+            foreach (var text in result)
+            {
+                tbResult.Text += text + Environment.NewLine;
+            }
+
+        }
+
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.RootFolder = Environment.SpecialFolder.MyComputer;
+            folder.SelectedPath = Path.GetDirectoryName(Application.ExecutablePath);
+            folder.ShowNewFolderButton = false;
+
+            if (folder.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                searchPath = folder.SelectedPath;
+                btnSearch.Enabled = true;
+                this.Text = searchPath;
+            }
+        }
+
+    }
+}
