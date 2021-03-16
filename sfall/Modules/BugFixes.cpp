3d7103ca77d43bdbc6a12e48d1ce65837cfa572d
Mr.Stalin <Fakels@yandex.ru>
2021-03-16 03:58:28 +0300

    Fixed game crash.

--- sfall/Modules/BugFixes.cpp
+++ sfall/Modules/BugFixes.cpp
@@ -2942,11 +2942,15 @@ skip:
 }
 
 static void __declspec(naked) op_create_object_sid_hack() {
+	using fo::Scripts::start;
 	__asm {
 		mov  ebx, [esp + 0x50 - 0x20 + 4]; // createObj
-		mov  edx, 1; // 'start' procedure
+		test ebx, ebx;
+		jz   noObject;
+		mov  edx, start; // procedure
 		mov  eax, [ebx + scriptId];
 		call fo::funcoffs::exec_script_proc_;
+noObject:
 		mov  edx, ebx;
 		mov  eax, esi;
 		retn;
@@ -3719,7 +3723,7 @@ void BugFixes::init()
 	MakeJump(0x4B6C3B, checkAllRegions_hack);
 	HookCall(0x4B6C13, checkAllRegions_hook);
 
-	// Fix a not-running the attached script of the object being created
+	// Fix the script attached to an object not being initialized (not run) properly upon object creation
 	MakeCall(0x4551C0, op_create_object_sid_hack, 1);
 }
 
