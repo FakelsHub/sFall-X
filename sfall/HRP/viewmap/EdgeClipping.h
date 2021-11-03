cf989f86064a9c538f3188169e091b5aa906025e
Mr.Stalin <Fakels@yandex.ru>
2021-11-03 19:45:41 +0300

    [HRP] Added edge map clipping (partial).

---
+++ sfall/HRP/viewmap/EdgeClipping.h
@@ -0,0 +1,19 @@
+/*
+ *    sfall
+ *    Copyright (C) 2008 - 2021 Timeslip and sfall team
+ *
+ */
+
+#pragma once
+
+namespace sfall
+{
+
+class EdgeClipping {
+
+public:
+	static void init();
+
+};
+
+}
